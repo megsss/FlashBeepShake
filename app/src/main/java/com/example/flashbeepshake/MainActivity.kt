5c8d681af93f15f0f3d0cda6e7758d9f678df8f3
Megan Stucky <mstucky1@msudenver.edu>
2020-05-03 11:13:19 -0600

    fixed action functions

--- app/src/main/java/com/example/flashbeepshake/MainActivity.kt
+++ app/src/main/java/com/example/flashbeepshake/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.flashbeepshake
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
+import android.widget.Button
+import android.widget.Switch
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,21 +33,69 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        flashlightswitch.setOnClickListener {
+            flash()
+        }
+
+        beepbutton.setOnClickListener {
+            beep()
+        }
+
+        shakebutton.setOnClickListener {
+            vibrate()
+        }
+
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    private fun vibrate() {
+        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+            print(vibrator.hasVibrator())
+        } else {
+            vibrator.vibrate(500);
+            print(vibrator.hasVibrator())
+        }
+    }
+
+    private fun beep() {
+        val tone = ToneGenerator(AudioManager.STREAM_RING, 200)
+        tone.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 500)
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun flash() {
+        val camera = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val id = camera.cameraIdList[0]
+        val flashbutton = findViewById<Switch>(R.id.flashlightswitch)
+        if (flashbutton.isChecked) {
+            camera.setTorchMode(id, true)
+        } else
+            camera.setTorchMode(id, false)
+    }
+
+
+
+}
+
+    //override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    //    menuInflater.inflate(R.menu.menu_main, menu)
+    //    return true
+    //}
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    //override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-}
+     //   return when (item.itemId) {
+    //        R.id.action_settings -> true
+    //        else -> super.onOptionsItemSelected(item)
+     //   }
+    //}
+
